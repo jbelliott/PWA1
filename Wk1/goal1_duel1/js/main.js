8a2d5701e98821986cdb3c6fa2b4ac49bd9f66cb
JB Elliott <jb.elliott@yahoo.com>
2014-04-03 20:48:59 -0400

    Started fight function.

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -16,19 +16,35 @@
  var playerTwoHealth - 100;
  var playerTwoDamage = 50;
 
- //Functions for fight and winner check
+ //Function for winner check
 
- function winnerCheck(){                                 //checks to see if the duel will continue
-     var winner = "Fight in Progress";
-     if (playerOneHealth <= 0 && playerTwoHealth <= 0){  //are both players dead?
+ var "Fight in Progress";
+ function winnerCheck(){                                    //checks to see if the duel will continue
+     if (playerOneHealth <= 0 && playerTwoHealth <= 0){     //are both players dead?
     winner = "Both Duelists have died.";
-     }else if (playerTwoHealth <= 0){                    //if not, is player two dead?
+     }else if (playerTwoHealth <= 0){                       //if not, is player two dead?
          winner = playerOneName + " is the winner!";
-     }else if (playerOneHealth <=0){                     //if not, is player one dead?
+     }else if (playerOneHealth <=0){                        //if not, is player one dead?
          winner = playerTwoName + " is the winner!";
      }
-     return winner;                                     //send result to variable "winner"
+     return winner;                                         //send result to variable "winner"
  }
 
+//Function for the Fight
 
- //Loop to contain functions
+ var round = 0;                                             //set round to begin at 0
+ function fight() {
+
+    //This alert starts the fight sequence
+    alert(playerOneName + ":" + playerOneHealth + " ** Start ** " + playerTwoName + ":" + playerTwoHealth);
+
+    while(winner === "Fight in Progress"){                  //loop to run until winner variable changes
+
+
+        alert(playerOneName + ": " + playerOneHealth + " ** Round " + round + " Over ** " + playerTwoName + ": " + playerTwoHealth);
+        round++;
+        winnerCheck();
+    }
+
+    alert(winner);
+ }
