e61d5e3e61a963b7fe51e6c9bb1ff8a07054d87c
JB Elliott <jb.elliott@yahoo.com>
2014-04-03 20:30:44 -0400

    Added starting values for duelists and function to check for winner.

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -4,12 +4,31 @@
  */
 
 
- //Define Variables
+ //Define Starting Variables, Player One
 
+ var playerOneName = "Spiderman";
+ var playerOneHealth = 100;
+ var playerOneDamage = 50;
 
+ //Define Starting Variables, Player Two
+
+ var playerTwoName = "Batman";
+ var playerTwoHealth - 100;
+ var playerTwoDamage = 50;
 
  //Functions for fight and winner check
 
+ function winnerCheck(){                                 //checks to see if the duel will continue
+     var winner = "Fight in Progress";
+     if (playerOneHealth <= 0 && playerTwoHealth <= 0){  //are both players dead?
+    winner = "Both Duelists have died.";
+     }else if (playerTwoHealth <= 0){                    //if not, is player two dead?
+         winner = playerOneName + " is the winner!";
+     }else if (playerOneHealth <=0){                     //if not, is player one dead?
+         winner = playerTwoName + " is the winner!";
+     }
+     return winner;                                     //send result to variable "winner"
+ }
 
 
  //Loop to contain functions
