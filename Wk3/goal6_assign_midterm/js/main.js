702734cc84b721b41276f95f67515e709d10540c
JB Elliott <jb.elliott@yahoo.com>
2014-04-17 23:55:21 -0400

    Created a function to move data from one existing object and add it to another.  Practical application, it adds a third student's data to the student object from a predefined new student object.

--- Wk3/goal6_assign_midterm/js/main.js
+++ Wk3/goal6_assign_midterm/js/main.js
@@ -6,25 +6,52 @@
 
 //Define the student variable and assign values for two students to start with
 
-var student = {};
+var classList= {};
 
-student.name1 = "Gomer Pyle";
-student.addy1 = {};
-student.addy1.street = "123 Sesame St";
-student.addy1.city = "Orlando";
-student.addy1.state = "FL";
-student.gpa1 = [2.5, 3.0, 3.0];
+classList.stu1 = {};
 
-student.name2 = "Fred Durst";
-student.addy2 = {};
-student.addy2.street = "404 Internet Blvd";
-student.addy2.city = "Miami";
-student.addy2.state = "FL";
-student.gpa2 = [3.5, 4.0, 3.5];
+classList.stu1.name = "Gomer Pyle";
+classList.stu1.addy1 = {};
+classList.stu1.addy1.street = "123 Sesame St";
+classList.stu1.addy1.city = "Orlando";
+classList.stu1.addy1.state = "FL";
+classList.stu1.gpa1 = [2.5, 3.0, 3.0];
+
+classList.stu2 = {};
+
+classList.stu2.name = "Fred Durst";
+classList.stu2.addy2 = {};
+classList.stu2.addy2.street = "404 Internet Blvd";
+classList.stu2.addy2.city = "Miami";
+classList.stu2.addy2.state = "FL";
+classList.stu2.gpa2 = [3.5, 4.0, 3.5];
 
 
-//Function to add values to the existing object
 
+var newStu = {};
+newStu.name = "Luke Skywalker";
+newStu.addy = {};
+newStu.addy.street = "5921 Echo Lake Way";
+newStu.addy.city = "Celebration";
+newStu.addy.state = "FL";
+newStu.newGpa = [4.0, 4.0, 3.5];
+
+//When called, this function will add the values from the newStu object to the classList object under stu3
+
+function addDate(name, newAddy1, newAddy2, newAddy3, grades){
+    classList.stu3 = {};
+
+    classList.stu3.name = name;
+    classList.stu3.addy3 = {};
+    classList.stu3.addy3.street = newAddy1;
+    classList.stu3.addy3.city = newAddy2;
+    classList.stu3.addy3.state = newAddy3;
+    classList.stu3.gpa3 = grades;
+
+    return classList.stu3;
+}
+
+addDate(newStu.name, newStu.addy.street, newStu.addy.city, newStu.addy.state, newStu.newGpa);
 
 
 
