2333e449934a5e1f8e1c6e43d933de5fd8e92f9e
JB Elliott <jb.elliott@yahoo.com>
2014-04-17 20:26:39 -0400

    Establishing commit.  Week 2 main.js file copies into js directory, all directories setup and ready to begin modifications.

---
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -0,0 +1,66 @@
+ /* Created by JB Elliott
+ Due April 3, 2014
+ The Duel - Part II
+ */
+
+     //Define Starting Variables, Player One, Array
+     //Array values are [name, health, max dmg, min dmg]
+     var playerOne = ["Spiderman", 100, 30, 15];
+
+
+     //Define Starting Variables, Player Two, Array
+     //Array values are [name, health, max dmg, min dmg]
+     var playerTwo = ["Batman", 100, 30, 15];
+
+     //Function for winner check
+
+     var winner = "Fight in Progress";
+
+     function winnerCheck() {                                    //checks to see if the duel will continue
+         if (playerOne[1] <= 0 && playerTwo[1] <= 0) {     //are both players dead?
+             winner = "Both Duelists have died.";
+         } else if (playerTwo[1] <= 0) {                       //if not, is player two dead?
+             winner = playerOne[0] + " is the winner!";
+         } else if (playerOne[1] <= 0) {                        //if not, is player one dead?
+             winner = playerTwo[0] + " is the winner!";
+         }
+         return winner;                                         //send result to variable "winner"
+     }
+
+//Function for the Fight
+
+     var round = 0;                                             //set round to begin at 0
+     function fight() {
+
+         //This alert starts the fight sequence, element 0 refers to player name, 1 refers to player health
+         alert(playerOne[0] + ":" + playerOne[1] + " ** Start ** " + playerTwo[0] + ":" + playerTwo[1]);
+         console.log(" ** Fight! ** ");
+         for (var i = 0; i < 10; i++) {                  //loop to run until duel ends
+
+             //How bad does it hurt this round? Array element 2 refers to max damage, 3 refers to min damage
+             var playerOneHit = Math.floor(Math.random() * (playerOne[2] - playerOne[3]) + playerOne[3]);
+             var playerTwoHit = Math.floor(Math.random() * (playerTwo[2] - playerTwo[3]) + playerTwo[3]);
+
+             //Apply the damage
+             playerOne[1] -= playerOneHit;
+             playerTwo[1] -= playerTwoHit;
+             round++;
+
+             //Show it in the console and alert
+             console.log(playerOne[0] + ": " + playerOne[1] + " ** Round " + round + " Over ** " + playerTwo[0] + ": " + playerTwo[1]);
+             alert(playerOne[0] + ": " + playerOne[1] + " ** Round " + round + " Over ** " + playerTwo[0] + ": " + playerTwo[1]);
+
+             winnerCheck();                                      //Everyone still standing?
+             if (winner != "Fight in Progress") {
+                 console.log(winner);
+
+                 break;
+             }
+         }
+
+         alert(winner);                                          //Someone died...
+     }
+
+ //To start it all off
+ fight();
+
