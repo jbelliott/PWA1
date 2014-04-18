2324b9c6b7d77c479d7e25de0604369f2a632a66
JB Elliott <jb.elliott@yahoo.com>
2014-04-17 23:20:03 -0400

    Created object variable and stored two students worth of data. Tested to ensure it was outputting properly.

--- Wk3/goal6_assign_midterm/js/main.js
+++ Wk3/goal6_assign_midterm/js/main.js
@@ -3,3 +3,29 @@
 	Due 18 April 2014
 
 */
+
+//Define the student variable and assign values for two students to start with
+
+var student = {};
+
+student.name1 = "Gomer Pyle";
+student.addy1 = {};
+student.addy1.street = "123 Sesame St";
+student.addy1.city = "Orlando";
+student.addy1.state = "FL";
+student.gpa1 = [2.5, 3.0, 3.0];
+
+student.name2 = "Fred Durst";
+student.addy2 = {};
+student.addy2.street = "404 Internet Blvd";
+student.addy2.city = "Miami";
+student.addy2.state = "FL";
+student.gpa2 = [3.5, 4.0, 3.5];
+
+
+//Function to add values to the existing object
+
+
+
+
+
