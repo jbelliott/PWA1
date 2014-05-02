536f1bb6e10c8e953d475da545bf86ee3988a4be
JB Elliott <jb.elliott@yahoo.com>
2014-05-02 16:52:12 -0400

    Added constructor to event button. Tested for correct output.

--- Wk4/goal7_assign_final/js/main.js
+++ Wk4/goal7_assign_final/js/main.js
@@ -5,26 +5,12 @@
 */
 
 
-(function(){
+//(function(){
 
 //Define the student variable and assign values for two students to start with
 
 
     /*var stu = {};
-
-    stu.name1 = "Gomer Pyle";
-    stu.addy1 = {};
-    stu.addy1.street1 = "123 Sesame St";
-    stu.addy1.city1 = "Orlando";
-    stu.addy1.state1 = "FL";
-    stu.gpa1 = [2.5, 3.0, 3.0];
-
-    stu.name2 = "Fred Durst";
-    stu.addy2 = {};
-    stu.addy2.street2 = "404 Internet Blvd";
-    stu.addy2.city2 = "Miami";
-    stu.addy2.state2 = "FL";
-    stu.gpa2 = [3.5, 4.0, 3.5];
 
 
     var newStu = {};
@@ -36,25 +22,8 @@
     newStu.newGpa = [4.0, 4.0, 3.5];
     */
 
-    //Changing student object to a constructor
-
-    var StudentInfo = function(name, street, cityState, gpa){
-        this.studentName = name;
-        this.studentStreet = street;
-        this.studentCity = cityState;
-        this.studentGPA = gpaAverage(gpa);
 
 
-
-    };
-
-    //Information to pass to the constructor
-    var students = [
-        new StudentInfo("Gomer Pyle", "123 Sesame St", "Orlando, FL", [2.5, 3.0, 3.0]),
-        new StudentInfo("Fred Durst", "404 Internet Blvd", "Miami, FL", [3.5, 4.0, 3.5])
-
-
-    ];
 
 
 /*
@@ -90,86 +59,67 @@
         arrayAvg = arrayAvg.toFixed(2);
         return arrayAvg;
     }
-/*
+
 //This is the function activated by the event button on the HTML page. It basically cycles through the student records
 //until it has reached the end, then disables the event button.
 
 
-    var event = 0;              //An event counter to sort the event arrays
+    var i = 0;              //An event counter to sort the event arrays
 
-        onclick = function studentList() {
-        var localDate = new Date();
+        onclick = function nextStudent() {
+            var localDate = new Date();
+            document.getElementById("date").innerHTML = "Date: " + localDate.toLocaleDateString();
+            console.log("Date: " + localDate.toLocaleDateString());
+            console.log("-------------------");
 
-        passNames(event);
-        passAddy(event);
-        passGPA(event);
-        document.getElementById("date").innerHTML = "Date: " + localDate.toLocaleDateString();
-        console.log("Date: " + localDate.toLocaleDateString());
-        console.log("-------------------");
-        event++;
-        //This disables the button when the maximum number of clicks is reached.
-        if (event > 2){
-            document.getElementById("button").innerHTML = "Done!";
-            console.log("No more entries to display.");
-            this.onclick=null;
-        }
+
+
+
+            //Changing student object to a constructor
+            console.log("test round " + i);
+
+            var StudentInfo = function (name, street, cityState, gpa) {
+                this.studentName = name;
+                console.log(this.studentName);
+                document.getElementById("name").innerHTML = "Name: " + this.studentName;
+                this.studentStreet = street;
+                console.log(this.studentStreet);
+                this.studentCity = cityState;
+                console.log(this.studentCity);
+                document.getElementById("address").innerHTML = "Address: " + this.studentStreet + ", " + this.studentCity;
+                this.studentGrades = gpa;
+                console.log(this.studentGrades);
+                document.getElementById("gpa").innerHTML = "GPA: " + this.studentGrades;
+                this.studentGPA = gpaAverage(gpa);
+                console.log(this.studentGPA);
+                document.getElementById("gpaavg").innerHTML = "GPA Avg: " + this.studentGPA;
+
+            };
+
+            //Information to pass to the constructor
+            var newStudent;
+            if (i == 0) {
+            newStudent = new StudentInfo("Gomer Pyle", "123 Sesame St", "Orlando, FL", [2.5, 3.0, 3.0]);
+                i++;
+            } else if (i == 1) {
+            newStudent = new StudentInfo("Fred Durst", "404 Internet Blvd", "Miami, FL", [3.5, 4.0, 3.5]);
+                i++;
+            } else if (i == 2) {
+                newStudent = new StudentInfo("Luke Skywalker", "5921 Echo Lake Way", "Celebration, FL", [4.0, 4.0, 3.5]);
+                document.getElementById("button").innerHTML = "Done!";
+                console.log("No more entries to display.");
+                this.onclick=null;
+            }
+
+
+console.log(i);
+
+
+
+
+
+
+
 
     };
-
-    //This function utilizes the event variable as a counter to determine which block of code is going to be called.
-    //As the name suggests, it passes the name of the student to the document
-    function passNames(counter) {
-
-        if (counter == 0) {
-            document.getElementById("name").innerHTML = "Name: " + stu.name1;
-            console.log("Name: " + stu.name1);
-        } else if (counter == 1) {
-            document.getElementById("name").innerHTML = "Name: " + stu.name2;
-            console.log("Name: " + stu.name2);
-        } else if (counter ==2) {
-            document.getElementById("name").innerHTML = "Name: " + stu.name3;
-            console.log("Name: " + stu.name3);
-        }
-    }
-    //This function utilizes the event variable as a counter to determine which block of code is going to be called.
-    //As the name suggests, it passes the address of the student to the document
-    function passAddy(counter) {
-
-        if (counter == 0) {
-            document.getElementById("address").innerHTML = "Address: " + stu.addy1.street1 + ", " + stu.addy1.city1 + ", " + stu.addy1.state1;
-            console.log("Address: " + stu.addy1.street1 + ", " + stu.addy1.city1 + ", " + stu.addy1.state1);
-        } else if (counter == 1) {
-            document.getElementById("address").innerHTML = "Address: " + stu.addy2.street2 + ", " + stu.addy2.city2 + ", " + stu.addy2.state2;
-            console.log("Address: " + stu.addy2.street2 + ", " + stu.addy2.city2 + ", " + stu.addy2.state2);
-        } else if (counter ==2) {
-            document.getElementById("address").innerHTML = "Address: " + stu.addy3.street3 + ", " + stu.addy3.city3 + ", " + stu.addy3.state3;
-            console.log("Address: " + stu.addy3.street3 + ", " + stu.addy3.city3 + ", " + stu.addy3.state3);
-        }
-    }
-
-    //This function utilizes the event variable as a counter to determine which block of code is going to be called.
-    //As the name suggests, it passes the GPA of the student to the document
-    function passGPA(counter) {
-
-        if (counter == 0) {
-            document.getElementById("gpa").innerHTML = "GPA: " + stu.gpa1[0] + ", " + stu.gpa1[1] + ", " + stu.gpa1[2];
-            console.log("GPA: " + stu.gpa1[0] + ", " + stu.gpa1[1] + ", " + stu.gpa1[2]);
-            document.getElementById("gpaavg").innerHTML = "GPA Avg: " + gpaAverage(stu.gpa1);
-            console.log("GPA Avg: " + gpaAverage(stu.gpa1));
-        } else if (counter == 1) {
-            document.getElementById("gpa").innerHTML = "GPA: " + stu.gpa2[0] + ", " + stu.gpa2[1] + ", " + stu.gpa2[2];
-            console.log("GPA: " + stu.gpa2[0] + ", " + stu.gpa2[1] + ", " + stu.gpa2[2]);
-            document.getElementById("gpaavg").innerHTML = "GPA Avg: " + gpaAverage(stu.gpa2);
-            console.log("GPA Avg: " + gpaAverage(stu.gpa2));
-        } else if (counter ==2) {
-            document.getElementById("gpa").innerHTML = "GPA: " + stu.gpa3[0] + ", " + stu.gpa3[1] + ", " + stu.gpa3[2];
-            console.log("GPA: " + stu.gpa3[0] + ", " + stu.gpa3[1] + ", " + stu.gpa3[2]);
-            document.getElementById("gpaavg").innerHTML = "GPA Avg: " + gpaAverage(stu.gpa3);
-            console.log("GPA Avg: " + gpaAverage(stu.gpa3));
-        }
-    }
-
-*/
-//
-
-})();
+//})();
